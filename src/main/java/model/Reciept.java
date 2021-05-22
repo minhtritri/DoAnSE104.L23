792d03b6b943862835290b87d0cd66d7370ffdfa
thaongan251 <thaongan251@gmail.com>
2021-05-22 11:23:05 +0700

    readme

--- src/main/java/model/Reciept.java
+++ src/main/java/model/Reciept.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-/**
- *
- * @author anhha
- */
+import java.util.Date;
+
 public class Reciept {
     private String sMaPBH;
     private String sMaKH;
