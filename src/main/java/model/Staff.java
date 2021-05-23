a7f06e04f70942eb023cac65adbc9984395c4b29
8yperc <19522539@gm.uit.edu.vn>
2021-05-23 20:49:25 +0700

    class guest and staff

--- src/main/java/model/Staff.java
+++ src/main/java/model/Staff.java
@@ -1,5 +1,90 @@
 package model;
 
+import java.util.Date;
+
 public class Staff {
+    private String MaNV;
+    private String HoTen;
+    private Date NgaySinh;
+    private String GioiTinh;
+    private String DiaChi;
+    private String SDT;
+    private Date NGAYVL;
+    private int CALV;
+
+    public Staff(String MaNV, String HoTen, Date NgaySinh, String GioiTinh, String DiaChi, String SDT, Date NGAYVL, int CALV) {
+        this.MaNV = MaNV;
+        this.HoTen = HoTen;
+        this.NgaySinh = NgaySinh;
+        this.GioiTinh = GioiTinh;
+        this.DiaChi = DiaChi;
+        this.SDT = SDT;
+        this.NGAYVL = NGAYVL;
+        this.CALV = CALV;
+    }
+
+    public String getMaNV() {
+        return MaNV;
+    }
+
+    public String getHoTen() {
+        return HoTen;
+    }
+
+    public Date getNgaySinh() {
+        return NgaySinh;
+    }
+
+    public String getGioiTinh() {
+        return GioiTinh;
+    }
+
+    public String getDiaChi() {
+        return DiaChi;
+    }
+
+    public String getSDT() {
+        return SDT;
+    }
+
+    public Date getNGAYVL() {
+        return NGAYVL;
+    }
+
+    public int getCALV() {
+        return CALV;
+    }
+
+    public void setMaNV(String MaNV) {
+        this.MaNV = MaNV;
+    }
+
+    public void setHoTen(String HoTen) {
+        this.HoTen = HoTen;
+    }
+
+    public void setNgaySinh(Date NgaySinh) {
+        this.NgaySinh = NgaySinh;
+    }
+
+    public void setGioiTinh(String GioiTinh) {
+        this.GioiTinh = GioiTinh;
+    }
+
+    public void setDiaChi(String DiaChi) {
+        this.DiaChi = DiaChi;
+    }
+
+    public void setSDT(String SDT) {
+        this.SDT = SDT;
+    }
+
+    public void setNGAYVL(Date NGAYVL) {
+        this.NGAYVL = NGAYVL;
+    }
+
+    public void setCALV(int CALV) {
+        this.CALV = CALV;
+    }
     
 }
