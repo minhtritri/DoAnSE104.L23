c4841d2383cd2f664cc019bb2563a1eca2cfe27a
Triết <62046776+minhtriet2507@users.noreply.github.com>
2021-05-23 21:57:41 +0700

    Update DrugstoreReceipt.java

--- src/main/java/model/DrugstoreReciept.java
+++ src/main/java/model/DrugstoreReciept.java
@@ -1,5 +1,82 @@
 package model;
 
+import java.util.Date;
+
 public class DrugstoreReciept {
+    private String sMAMH;
+    private String sMATHUOC;
+    private String sMANV;
+    private String sMANCC;
+    private int iSL;
+    private float fTONGGT;
+    private Date dNGAYMUA;
     
+    public DrugstoreReciept(String sMAMH, String sMATHUOC, String sMANV,String sMANCC, int iSL, float fTONGGT, Date dNGAYMUA) {
+        this.sMAMH = sMAMH;
+        this.sMATHUOC = sMATHUOC;
+        this.sMANV = sMANV;
+        this.sMANCC = sMANCC;
+        this.iSL = iSL;
+        this.fTONGGT = fTONGGT;
+        this.dNGAYMUA = dNGAYMUA;
+    }
+    
+    public DrugstoreReciept() {
+    }
+    
+    public String getsMAMH() {
+        return sMAMH;
+    }
+
+    public void setsMAMH(String sMAMH) {
+        this.sMAMH = sMAMH;
+    }
+
+    public String getsMATHUOC() {
+        return sMATHUOC;
+    }
+
+    public void setsMATHUOC(String sMATHUOC) {
+        this.sMATHUOC = sMATHUOC;
+    }
+
+    public String getsMANV() {
+        return sMANV;
+    }
+
+    public void setsMANV(String sMANV) {
+        this.sMANV = sMANV;
+    }
+
+    public String getsMANCC() {
+        return sMANCC;
+    }
+
+    public void setsMANCC(String sMANCC) {
+        this.sMANCC = sMANCC;
+    }
+
+    public int getiSL() {
+        return iSL;
+    }
+
+    public void setiSL(int iSL) {
+        this.iSL = iSL;
+    }
+
+    public float getfTONGGT() {
+        return fTONGGT;
+    }
+
+    public void setfTONGGT(float fTONGGT) {
+        this.fTONGGT = fTONGGT;
+    }
+
+    public Date getdNGAYMUA() {
+        return dNGAYMUA;
+    }
+
+    public void setdNGAYMUA(Date dNGAYMUA) {
+        this.dNGAYMUA = dNGAYMUA;
+    }
 }
