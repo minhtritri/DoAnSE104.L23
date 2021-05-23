8cbc12f25bd846e4f64d98e5ebee7336df5a461f
Triết <62046776+minhtriet2507@users.noreply.github.com>
2021-05-23 21:58:54 +0700

    Update InvoiceDetails.java

--- src/main/java/model/InvoiceDetails.java
+++ src/main/java/model/InvoiceDetails.java
@@ -1,5 +1,39 @@
 package model;
 
 public class InvoiceDetails {
+    private String sMABH;
+    private String sMATHUOC;
+    private int iSL;
     
+    public InvoiceDetails (String sMABH, String sMATHUOC, int iSL) {
+        this.sMABH = sMABH;
+        this.sMATHUOC = sMATHUOC;
+        this.iSL = iSL;
+    }
+    public InvoiceDetails () {  
+    }
+
+    public String getsMABH() {
+        return sMABH;
+    }
+
+    public void setsMABH(String sMABH) {
+        this.sMABH = sMABH;
+    }
+
+    public String getsMATHUOC() {
+        return sMATHUOC;
+    }
+
+    public void setsMATHUOC(String sMATHUOC) {
+        this.sMATHUOC = sMATHUOC;
+    }
+
+    public int getiSL() {
+        return iSL;
+    }
+
+    public void setiSL(int iSL) {
+        this.iSL = iSL;
+    }
 }
